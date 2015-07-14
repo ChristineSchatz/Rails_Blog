cb174a7616a0e544d95d7a32f2e6235b23d33dcd
Christine Schatz & Vic Zhu <pair+ChristineSchatz.sayuloveit@devbootcamp.com>
2015-07-14 12:13:14 -0400

    fin user signup and login

--- rails_blog/app/models/user.rb
+++ rails_blog/app/models/user.rb
@@ -1,7 +1,10 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+
   has_secure_password
   has_many :blogs
 
   validates :username, uniqueness: { case_sensitive: false }
   validates :username, :password, presence: true
+  include BCrypt
 end
